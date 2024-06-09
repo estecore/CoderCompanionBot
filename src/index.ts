3b0a9cca062ad326dcbd14bc874cb3023909755f
estecore <estecore@yandex.ru>
2024-06-09 16:50:46 +0400

    feat(index.ts, /helpers): create handleReminderFunction

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import TelegramBot from "node-telegram-bot-api";
+import TelegramBot, { InlineKeyboardMarkup } from "node-telegram-bot-api";
 import * as dotenv from "dotenv";
+
+import handleReminderFunction from "./helpers/reminderFunc";
 
 dotenv.config();
 
@@ -11,11 +13,47 @@ if (!token) {
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start|Start|Старт/, (msg) => {
+bot.onText(/\/start|start|старт/i, (msg) => {
+  const keyboard: InlineKeyboardMarkup = {
+    inline_keyboard: [
+      [
+        { text: "Напоминания", callback_data: "reminders" },
+        { text: "Упражнения", callback_data: "exercises" },
+      ],
+      [{ text: "Остаток дней жизни", callback_data: "lifecount" }],
+    ],
+  };
+
   bot.sendMessage(
     msg.chat.id,
-    "Привет! Я ваш бот-помощник. Я буду помогать вам отслеживать перерывы для глаз, делать физические упражнения и даже отсчитывать оставшиеся дни вашей жизни. Используйте /setreminder, /setbirthdate и /lifecount для настройки напоминаний и просмотра оставшегося времени."
+    `
+*Привет!* Я ваш *Бот-помощник*. 
+Я буду помогать вам отслеживать перерывы для глаз, делать физические упражнения 
+и даже отсчитывать оставшиеся дни вашей жизни.
+
+Пожалуйста, выберите интересующую вас функцию:
+    `,
+    {
+      parse_mode: "Markdown",
+      reply_markup: keyboard,
+    }
   );
+});
+
+bot.on("callback_query", (callbackQuery) => {
+  if (!callbackQuery.message) {
+    return;
+  }
+
+  const data = callbackQuery.data;
+  const chatId = callbackQuery.message.chat.id;
+
+  switch (data) {
+    case "reminders": {
+      handleReminderFunction(bot, chatId);
+      break;
+    }
+  }
 });
 
 console.log("Bot is running!");
